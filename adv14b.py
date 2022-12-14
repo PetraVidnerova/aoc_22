b39e4ce9b890f56170bacad5e83edd81afabea4f
Petra Vidnerova <petra.vidnerova@gmail.com>
2022-12-14 12:11:22 +0100

    day 14, slow (due to recursion)

---
+++ adv14b.py
@@ -0,0 +1,66 @@
+blocks = set()
+bottomline = 0
+
+
+def parse_line(line):
+    global blocks, bottomline
+
+    points = []
+    for point in line.split("->"):
+        x, y = map(int, point.split(","))
+        points.append((x, y))
+
+    start = 0
+    end = 1
+    while end < len(points):
+        s = points[start]
+        e = points[end]
+        if s[0] == e[0]:
+            if s[1] > e[1]:
+                s, e = e, s
+            if e[1] > bottomline:
+                bottomline = e[1]
+            for i in range(s[1], e[1]+1):
+                blocks.add((s[0], i))
+        elif s[1] == e[1]:
+            if s[0] > e[0]:
+                s, e = e, s
+            if s[1] > bottomline:
+                bottomline = s[1]
+            for i in range(s[0], e[0]+1):
+                blocks.add((i, s[1]))
+        start, end = start+1, end+1
+
+
+def fall(x, y):
+    global blocks, bottomline
+
+    if y < (bottomline-1):
+        for next_pos in (
+                (x, y+1),
+                (x-1, y+1),
+                (x+1, y+1)
+        ):
+            if next_pos not in blocks:
+                fall(*next_pos)
+                return
+    blocks.add((x, y))
+
+
+with open("input14.txt", "r") as f:
+    for line in f:
+        parse_line(line)
+
+
+bottomline += 2
+source = (500, 0)
+
+i = 0
+while True:
+    i += 1
+    fall(*source)
+    if source in blocks:
+        break
+
+print("END")
+print(i)
